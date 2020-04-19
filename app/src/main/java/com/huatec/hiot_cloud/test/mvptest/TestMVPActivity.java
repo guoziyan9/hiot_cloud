fcfb1c511a4a9641b28cefedd60585ac4184294e
guoziyan <1615991390@qq.com>
2020-04-19 11:01:05 +0800

    增加MVP模板类，修改业务类调用方式

--- app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestMVPActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestMVPActivity.java
@@ -9,19 +9,26 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.huatec.hiot_cloud.R;
+import com.huatec.hiot_cloud.base.BaseActivity;
 import com.huatec.hiot_cloud.test.mvptest.model.User;
 
-public class TestMVPActivity extends AppCompatActivity implements TestView{
+public class TestMVPActivity extends BaseActivity<TestView, TestPresenter> implements TestView{
+
+    private TestPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        presenter = new TestPresenter();
         super.onCreate( savedInstanceState );
+
         setContentView( R.layout.activity_test_mvp );
         final EditText etUserName = findViewById( R.id.et_user_name );
         final EditText etPassword = findViewById( R.id.et_password );
         Button btnLogin = findViewById( R.id.btn_login );
         final User user = new User();
-        final TestPresenter presenter = new TestPresenter(this);
+
         btnLogin.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,9 +36,14 @@ public class TestMVPActivity extends AppCompatActivity implements TestView{
                 user.setUserName(etUserName.getText().toString());
                 user.setPassword(etPassword.getText().toString());
                 presenter.login( user );
-            //    login(user);
+
             }
         } );
+    }
+
+    @Override
+    public TestPresenter createPresenter() {
+        return presenter;
     }
 
     @Override
--- app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestPresenter.java
+++ app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestPresenter.java
@@ -1,19 +1,23 @@
 package com.huatec.hiot_cloud.test.mvptest;
 
+import com.huatec.hiot_cloud.base.BasePresenter;
 import com.huatec.hiot_cloud.test.mvptest.model.User;
 
-public class TestPresenter {
+public class TestPresenter extends BasePresenter<TestView> {
 
-    private TestView view;
-    public TestPresenter(TestView view) {
-this.view = view;
+
+
+    public TestPresenter() {
+
     }
-    public void login(User user){
-        if ("guoziyan".equals( user.getUserName() )&&"123".equals( user.getPassword() )){
-           view.showMessage( "登录成功" );
-        }
-        else {
-            view.showMessage( "登录失败" );
+
+
+    public void login(User user) {
+        if ("guoziyan".equals( user.getUserName() ) && "123".equals( user.getPassword() )) {
+            getView().showMessage( "登录成功" );
+        } else {
+            getView().showMessage( "登录失败" );
         }
     }
+
 }
